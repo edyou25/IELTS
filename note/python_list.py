fc95abba7b84a83ddc1b1fd72c0781e6ed92f708
edward <edward.you@polyu.edu.hk>
2025-07-18 17:53:29 +0800

    Update vocabulary entries related to environment and resources

--- note/python_list.py
+++ note/python_list.py
@@ -400,5 +400,14 @@ words = [
   "waste management", "sanitation", "stormwater management", "green infrastructure",
   "urban green space"
 ]
+# 第一次听写错误 1-41
+words = ["fauna", "sewage", "litter", "smog", "hazardous materials", "carbon dioxide", "flood",
+    "heatwave", "melting ice caps", "natural resources", "non-renewable resources", "natural gas", "wind energy",
+    "renewable energy", "protect", "nature conservation", "afforestation", "habitat restoration", "eco-friendly",
+    "biodegradable", "compost", "circular economy", "zero waste", "dispose of", "emit",
+    "reduce emissions", "legislation", "pressure group", "non-governmental organization", "NGO", "Paris Agreement",
+    "environmental ethics", "carbon neutrality", "environmental degradation", "water scarcity", "desertification", "coral reef",
+    "industrialization", "stormwater management", "green infrastructure", "urban green space"
+]
 
 print("Total words:", len(words))
